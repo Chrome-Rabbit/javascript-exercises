2e9f7b5f6dc89fbc278b64d7bbbdd0988f723b9f
Chrome-Rabbit <cyber.bunny.rabbit@gmail.com>
2025-03-18 22:24:04 -0400

    Add answer to 01+02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,47 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+
+let counter = 0;
+let result = "";
+
+/* let originalString = string; */
+/* Attempt at a for loop
+for (let i = 0; i < num -1; i++){
+    string += originalString;
+}
+
+if (num <= -1){
+    return "ERROR"
+}
+
+if (num === 0){
+    return ""
+}
+
+    return string;
+*/
+
+while (true){
+
+    if(num < 0){
+        let result = "ERROR";
+        return result;
+    }
+
+    if(num === 0){
+        return result;
+    }
+
+    if(counter < num){
+        result += string;
+        counter++;
+    }
+    else{
+        return result;
+    }
+
+}
+
 
 };
 
