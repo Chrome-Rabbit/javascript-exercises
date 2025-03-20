46ca5bda3ba1796229436eaec08847e8148e9297
Chrome-Rabbit <cyber.bunny.rabbit@gmail.com>
2025-03-19 23:14:39 -0400

    Add sumAll() solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let finalSum = 0;
+
+    let min = Math.min(int1, int2);
+    let max = Math.max(int1, int2);
+
+    if(typeof int1 !== "number" || typeof int2 !== "number" || int1 < 0 || int2 < 0 || Number.isInteger(int1) === false || Number.isInteger(int2) === false ){
+        return "ERROR"
+    }
+
+    for(let i = min; i <= max; i++){
+        finalSum += i;
+    }
+
+
+    return finalSum;
 };
 
 // Do not edit below this line
