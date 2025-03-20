e7f7bae7f1d7d773d4cd084351ef966466d7060c
Chrome-Rabbit <cyber.bunny.rabbit@gmail.com>
2025-03-20 12:23:45 -0400

    Add leapYears() solution to 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(yearGiven) {
+
+    let condition1 = (yearGiven % 4 === 0);
+    let condition2 = (yearGiven % 100 === 0);
+    let condition3 = (yearGiven % 400 === 0);
+
+    if(condition1 && !condition2 || condition3){
+        return true;
+    }else{
+        return false;
+    }
 
 };
 
