f049a032676bb37297986cf629f4e9ae7520fd51
Chrome-Rabbit <cyber.bunny.rabbit@gmail.com>
2025-03-20 21:37:56 -0400

    Add temp conversion funcs solutions to 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenNumber) {
+
+  let celsiusConversion = ((5/9) * (givenNumber - 32));
+
+  return parseFloat(celsiusConversion.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenNumber) {
+  
+  let fahrenheitConversion = ((9/5) * givenNumber + 32);
+
+  return parseFloat(fahrenheitConversion.toFixed(1));
 };
 
 // Do not edit below this line
